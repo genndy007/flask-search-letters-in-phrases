991973000d179013eabc47dcc4402251f0544a79
genndy007 <kochevgenazp@gmail.com>
2020-12-11 20:47:39 +0200

    Commented main file for easiness of checking'

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
+# Flask is main object
+# render_template for returning html pages with jinja inserting
+# request is for handling form parameters filled by user
 from search import search4letters
+# our function for finding letters
 
 
 app = Flask(__name__)
 
+# starting point, here we take prompt
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template(
+        'entry.html',
+        the_title='Welcome to Estus Letter Search!'
+    )
 
+# final destination and results showing
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
@@ -18,15 +31,6 @@ def do_search() -> 'html':
         the_phrase=phrase,
         the_letters=letters,
         the_results=found
-    )
-
-
-@app.route('/')
-@app.route('/entry')
-def entry_page() -> 'html':
-    return render_template(
-        'entry.html',
-        the_title='Welcome to Estus Letter Search!'
     )
 
 
--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-# my function to deploy at the web
+# my function to deploy at the web - it searches for letters in a given phrase
 def search4letters(phrase: str, letters: str='aeiou') -> set:
     '''Returns set of letters found in phrase'''
     return set(letters).intersection(set(phrase))
