0da1214b9e57fbe96b1a88baac4e3e2fe6814764
genndy007 <kochevgenazp@gmail.com>
2020-12-11 19:53:02 +0200

    Wrote other routes functions, it is really interesting

--- main.py
+++ main.py
@@ -9,28 +9,28 @@ def hello() -> str:
     return 'Hello from Estus!'
 
 
-# @app.route('/search4', methods=['POST'])
-# def do_search() -> 'html':
-#     phrase = request.form['phrase']
-#     print(request.url)
-#     print(request.args)
-#     letters = request.form['letters']
-#     found = search4letters(phrase, letters)
-#     return render_template(
-#         'results.html',
-#         the_title='Estus results:',
-#         the_phrase=phrase,
-#         the_letters=letters,
-#         the_results=found
-#     )
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    print(request.url)
+    print(request.args)
+    letters = request.form['letters']
+    found = search4letters(phrase, letters)
+    return render_template(
+        'results.html',
+        the_title='Estus results:',
+        the_phrase=phrase,
+        the_letters=letters,
+        the_results=found
+    )
 
 
-# @app.route('/entry')
-# def entry_page() -> 'html':
-#     return render_template(
-#         'entry.html',
-#         the_title='Welcome to Estus Letter Search!'
-#     )
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template(
+        'entry.html',
+        the_title='Welcome to Estus Letter Search!'
+    )
 
 
 app.run(debug=True)
